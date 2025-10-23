5b55c67473ac27242c2f6fb8f87dcf449099d651
Ilyas Turki <turki.ilyass@gmail.com>
2025-10-23 14:12:12 +0200

    feat: use clouds theme

--- app/lib/editor/theme/theme-extension.ts
+++ app/lib/editor/theme/theme-extension.ts
@@ -1,14 +1,16 @@
-import { syntaxHighlighting } from '@codemirror/language'
+import { clouds } from 'thememirror'
 
-import { baseViewTheme } from './base-view-theme'
-import { markdownHighlightStyle } from './markdown-highlight-style'
-import { highlightStyle, viewTheme } from './theme'
+export const themeExtension = [clouds]
 
-export const themeExtension = [
-    baseViewTheme,
-
-    viewTheme,
-    syntaxHighlighting(highlightStyle),
-
-    syntaxHighlighting(markdownHighlightStyle),
-]
+// Previous custom theming (commented out for future reference):
+// import { syntaxHighlighting } from '@codemirror/language'
+// import { baseViewTheme } from './base-view-theme'
+// import { markdownHighlightStyle } from './markdown-highlight-style'
+// import { highlightStyle, viewTheme } from './theme'
+//
+// export const themeExtension = [
+//     baseViewTheme,
+//     viewTheme,
+//     syntaxHighlighting(highlightStyle),
+//     syntaxHighlighting(markdownHighlightStyle),
+// ]
