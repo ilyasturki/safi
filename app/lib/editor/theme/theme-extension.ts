368b0cdbc11244aa2270012de6627e5ada30f31b
Ilyas Turki <turki.ilyass@gmail.com>
2025-10-24 17:09:37 +0200

    refactor: remove unused code

--- app/lib/editor/theme/theme-extension.ts
+++ app/lib/editor/theme/theme-extension.ts
@@ -9,16 +9,3 @@ export const themeExtension = [
     syntaxHighlighting(markdownStyles),
     headingOutdentTheme,
 ]
-
-// Previous custom theming (commented out for future reference):
-// import { syntaxHighlighting } from '@codemirror/language'
-// import { baseViewTheme } from './base-view-theme'
-// import { markdownHighlightStyle } from './markdown-highlight-style'
-// import { highlightStyle, viewTheme } from './theme'
-//
-// export const themeExtension = [
-//     baseViewTheme,
-//     viewTheme,
-//     syntaxHighlighting(highlightStyle),
-//     syntaxHighlighting(markdownHighlightStyle),
-// ]
